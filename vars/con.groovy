b83593820a02118df01c864e709377e217cd3dfc
Kollurdilip98 <141479994+Kollurdilip98@users.noreply.github.com>
2023-09-30 12:29:58 +0530

    Update con.groovy

--- vars/con.groovy
+++ vars/con.groovy
@@ -7,3 +7,7 @@ def build()
 {
 sh 'mvn package'
 }
+def test(jobname,qa,dest)
+{
+sh 'scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${qa}:/var/lib/tomcat9/webapps/${dest}.war'
+}
